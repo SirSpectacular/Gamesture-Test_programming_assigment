9fb463ecdc977b930824e5a1fce1403d40391024
Szymon Czarny <sz.p.czarny@gmail.com>
2020-01-30 17:49:52 +0100

    Minimum viable product

---
+++ Assets/Scripts/FadingDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FadingDisplay : MonoBehaviour
+{
+    private float _timeToDisplay= 0.0f;
+    private TextMeshProUGUI _textMesh;
+
+    public float timeOfFading = 1.0f;
+
+    void Start()
+    {
+        _textMesh = GetComponent<TextMeshProUGUI>();
+        _textMesh.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    void Update()
+    {
+        if(_timeToDisplay <= timeOfFading)
+        {
+            _textMesh.color = new Color(1f, 1f, 1f, _timeToDisplay / timeOfFading);
+        }
+        _timeToDisplay -= Time.deltaTime;
+    }
+
+    public void Display(float seconds)
+    {
+        _textMesh.color = new Color(1f, 1f, 1f, 1f);
+        _timeToDisplay = seconds;
+    }
+}
